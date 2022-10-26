58e6f35ff35acc6f1c82dba3d2e7136fed0b9e2d
Paulo Monezi <paulo.monezi@icloud.com>
2022-10-25 20:26:55 -0400

    using style components framework lib

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
 import AppRoutes from './pages/routes'
+import { createGlobalStyle } from 'styled-components'
 
 function App() {
   return (
     <div>
+      <GlobalStyle/>
       <AppRoutes />
     </div>
   );
 }
+
+const GlobalStyle = createGlobalStyle`
+*
+{
+padding: 0;
+margin: 0;
+}
+`
 
 export default App;
